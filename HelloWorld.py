3a63e52da5fdcf8daf0ff93af4a231e6b840af36
NUR IZYANI <76443379+nurizyani@users.noreply.github.com>
2020-12-22 00:45:25 +0800

    "Hello, World"

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+import datetime
+now = datetime.datetime.now()
+print("Date : ", now.strftime("%d.%m.%Y"))
+print("Time : ", now.strftime("%H:%M\n"))
+
+print("*" * 19)
+print("\tHello, World")
+print("*" * 19)
+
+print("\nby Nurizyani")
